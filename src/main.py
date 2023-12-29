74cc476d6fdf0c16f432a5dff1bf94f80a519a55
James Green <james.green6@digital.justice.gov.uk>
2023-12-29 13:02:25 +0000

    handle nbsp

--- src/main.py
+++ src/main.py
@@ -11,11 +11,15 @@ def convert_csv_to_json(file_path):
     csv_rows = []
     with open(file_path) as csvfile:
         reader = csv.DictReader(csvfile, skipinitialspace=True, quotechar='"')
-        title = reader.fieldnames
 
+        # Here we clean any Non-breaking spaces and convert to normal spacing. \xa0 converts to ' '
         for row in reader:
-            csv_rows.extend([{title[i]: row[title[i]]
-                              for i in range(len(title))}])
+            temp_overwritable_dict = {}
+            for key, value in row.items():
+                cleaned_key = key.replace('\xa0', ' ')
+                cleaned_value = value.replace('\xa0', ' ')
+                temp_overwritable_dict.update({cleaned_key: cleaned_value})
+            csv_rows.append(temp_overwritable_dict)
 
     if csv_rows == None or csv_rows == []:
         raise ValueError('Failed to convert CSV file to JSON. Exiting script.')
